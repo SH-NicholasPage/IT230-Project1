a5b58b695419f76a523aadbaa08a4889905f1209
Nicholas Page <npage@southhills.edu>
2024-09-23 19:52:46 -0400

    Removed comments and the class definition

--- mySet.cpp
+++ mySet.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Nick on 10/2/2023.
-//
-
 #include "mySet.h"
--- mySet.h
+++ mySet.h
@@ -1,14 +1,7 @@
-//
-// Created by Nick on 10/2/2023.
-//
-
 #ifndef IT230_PROJECT1_MYSET_H
 #define IT230_PROJECT1_MYSET_H
 
-class mySet
-{
 
-};
 
 
 #endif //IT230_PROJECT1_MYSET_H
